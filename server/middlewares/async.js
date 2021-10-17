213ba41b6c580b9635b1b751a52ca2ae9d782c51
Aragor70 <mikey.prus@gmail.com>
2021-10-17 21:56:39 +0000

    Server update: js extenstions

--- server/middlewares/async.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (function (fn) { return function (req, res, next) {
-    Promise.resolve(fn(req, res, next)).catch(next);
-}; });
